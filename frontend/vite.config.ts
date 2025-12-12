a7ef28ab122e409025515628cd2e8d1595032cd6
ryouka0122 <coolblossom.0130@gmail.com>
2025-12-13 08:29:04 +0900

    frontendのCredential情報をパラメータ化

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,39 +4,54 @@ import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import path from "node:path";
+import {loadEnv} from "vite";
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
+import dotenvExpand from "dotenv-expand";
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const envData = dotenvExpand.expand({
+    parsed: {...env}
+  }).parsed ?? env;
+
+  return {
+    define: {
+      "import.meta.env": envData
     },
-  },
 
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "vuetify/styles" as *;`,
+    plugins: [
+      vue(),
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
 
-  server: {
-    port: 5173,
-    open: false,
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "vuetify/styles" as *;`,
+        },
+      },
+    },
 
-  // Vitestの設定
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./tests/setup.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx,js,jsx}"],
-  },
+    server: {
+      port: 5173,
+      open: false,
+    },
+
+    // Vitestの設定
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./tests/setup.ts"],
+      include: ["src/**/*.{test,spec}.{ts,tsx,js,jsx}"],
+    },
+  }
 });
