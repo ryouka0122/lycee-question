5f22ef9278063191b5141b33e8fc1c467d5b0995
ryouka0122 <coolblossom.0130@gmail.com>
2025-12-13 09:52:18 +0900

    CORS関連の設定値を整備

--- backend/src/main/java/net/lycee/web/enquete/config/WebConfig.java
+++ backend/src/main/java/net/lycee/web/enquete/config/WebConfig.java
@@ -5,6 +5,7 @@ import net.lycee.web.enquete.interceptor.LyceeAuthzInterceptor;
 import net.lycee.web.enquete.interceptor.LyceeLogInterceptor;
 import net.lycee.web.enquete.interceptor.LyceeTimeInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,6 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Autowired
     private LyceeApiKeyInterceptor lyceeApiKeyInterceptor;
@@ -41,7 +45,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins.split(","))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
